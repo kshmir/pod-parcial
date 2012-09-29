8a35ca07fa07fa002392983a740a67f7035fc65d
Cristian Pereyra <criis.pereyra@gmail.com>
2012-09-29 13:54:47 -0300

    cambios para que se cumpla lo que se pide en el ejercicio 1

--- src/main/java/org/kikis/pod/parcial2011/ej1/Request.java
+++ src/main/java/org/kikis/pod/parcial2011/ej1/Request.java
@@ -3,7 +3,7 @@ package org.kikis.pod.parcial2011.ej1;
 public class Request {
 
 	public void process() {
-
+		System.out.println("Lalala");
 	}
 
 }
--- src/main/java/org/kikis/pod/parcial2011/ej1/Server.java
+++ src/main/java/org/kikis/pod/parcial2011/ej1/Server.java
@@ -1,35 +1,88 @@
 package org.kikis.pod.parcial2011.ej1;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class Server extends Service {
 
 	protected DataBase dataBase;
 	protected DataBaseConnection dataBaseConnection;
 	protected FTPServer ftpServer;
 	protected WebServer webServer;
-
-	public Server() {
-		// TODO Auto-generated constructor stub
-	}
+	protected Thread worker;
+	protected final BlockingQueue<Request> requestQueue = new LinkedBlockingQueue<Request>(
+			1);
 
 	@Override
 	public void onInitialize() {
-		dataBase = new DataBase();
-		dataBase.initialize();
-		dataBaseConnection = new DataBaseConnection(dataBase);
-		dataBaseConnection.initialize();
-		ftpServer = new FTPServer();
-		ftpServer.initialize();
-		webServer = new WebServer();
-		webServer.initialize();
+
+		final ExecutorService service = Executors.newFixedThreadPool(3);
+
+		service.submit(new Callable<Boolean>() {
+			public Boolean call() throws Exception {
+				dataBase = new DataBase();
+				dataBase.initialize();
+				dataBaseConnection = new DataBaseConnection(dataBase);
+				dataBaseConnection.initialize();
+				return true;
+			}
+		});
+
+		service.submit(new Callable<Boolean>() {
+			public Boolean call() throws Exception {
+				ftpServer = new FTPServer();
+				ftpServer.initialize();
+				return true;
+			}
+		});
+
+		service.submit(new Callable<Boolean>() {
+			public Boolean call() throws Exception {
+				webServer = new WebServer();
+				webServer.initialize();
+				return true;
+			}
+		});
+
+		service.shutdown();
+
+		worker = new Thread(new Runnable() {
+			public void run() {
+				try {
+					service.awaitTermination(15, TimeUnit.MINUTES);
+
+					if (!(dataBase.isReady() && dataBaseConnection.isReady()
+							&& ftpServer.isReady() && webServer.isReady())) {
+						throw new IllegalStateException(
+								"Cannot process request if server is not ready");
+					}
+
+					boolean interrumpted = false;
+					while (!interrumpted || !requestQueue.isEmpty()) {
+						try {
+							requestQueue.take().process();
+						} catch (InterruptedException e) {
+							interrumpted = true;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		worker.start();
 	}
 
 	public void processRequest(final Request request) {
-		if (!(dataBase.isReady() && dataBaseConnection.isReady()
-				&& ftpServer.isReady() && webServer.isReady())) {
-			throw new IllegalStateException(
-					"Cannot process request if server is not ready");
+		try {
+			requestQueue.put(request);
+		} catch (InterruptedException e) {
 		}
-		request.process();
 	}
-
 }
